4b679388b6ea8ff3e34d6f54115a2066ac399b75
AnaKaroline-23 <anakarolinesoraes.23@gmail.com>
2024-09-25 14:08:35 -0300

    Atividade 21, programa que faz contagem regressiva de 10 até 0

---
+++ atividade21.py
@@ -0,0 +1,7 @@
+# Crie um programa que faça uma contagem regressiva de 10 até 0 e exiba "FIM!" ao final.
+from time import sleep 
+for número in range (10, -1, -1):
+ print (número)
+ sleep (0.5)
+print("FIM")
+
